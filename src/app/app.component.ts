dddefdf9aad52bb12fc799490099ca8ecba311f9
ilyasmohd <md.ilyas89@gmail.com>
2020-07-20 16:16:05 +0300

    side menu addition pending chagnes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController
   ) {
     this.initializeApp();
+    this.openFirst();
   }
 
   initializeApp() {
@@ -24,4 +27,11 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  openFirst() {
+    //this.menu.enable(false);
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
 }
